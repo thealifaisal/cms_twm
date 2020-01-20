fd470c14bdb405402d45fbc0e1f05503f407e0f4
Ali Faisal <alifaisalas99@gmail.com>
2020-01-21 02:08:48 +0500

    delete-team

---
+++ php/delete_team.php
@@ -0,0 +1,15 @@
+<?php
+
+include "connect_cms.php";
+
+$conn = openConn();
+
+// $team_id = intVal(trim($_GET["TID"]));
+$team_id = 1;
+
+$del_team = "DELETE FROM event_team WHERE team_id = $team_id";
+$result = $conn->query($del_team) or die("error: " . $conn->error);
+
+echo "1";
+
+?>
