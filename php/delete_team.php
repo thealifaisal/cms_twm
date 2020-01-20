d15d9d8b53dd64053136db4772cdcd6e0cef8b62
Ali Faisal <alifaisalas99@gmail.com>
2020-01-21 02:09:39 +0500

    no-change

--- php/delete_team.php
+++ php/delete_team.php
@@ -4,8 +4,8 @@ include "connect_cms.php";
 
 $conn = openConn();
 
-// $team_id = intVal(trim($_GET["TID"]));
-$team_id = 1;
+$team_id = intVal(trim($_GET["TID"]));
+// $team_id = 1;
 
 $del_team = "DELETE FROM event_team WHERE team_id = $team_id";
 $result = $conn->query($del_team) or die("error: " . $conn->error);
