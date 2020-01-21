3785232ae177b99b1b791580495eedde450c95be
Ali Faisal <alifaisalas99@gmail.com>
2020-01-22 01:25:53 +0500

    team-deleted

---
+++ js/delete_team.js
@@ -0,0 +1,24 @@
+
+function delTeam(){
+  var team_id = document.getElementById("team_id").value;
+
+  if(team_id == ""){
+    alert("Enter Team ID");
+    return 0;
+  }
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "http://192.168.1.102/SourceCode/php/delete_team.php?TID="+team_id, true);
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      if(this.responseText.includes("error")){
+        alert("Server-Side Error\nError: " + this.responseText);
+        return 0;
+      }
+      else{
+        alert("Success: Deleted Team No: "+team_id);
+      }
+    }
+  };
+  xhttp.send();
+}// delTeam()
