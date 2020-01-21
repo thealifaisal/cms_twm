fc063a0951211030f23fcbebbc2e1141a6fb3e0b
Ali Faisal <alifaisalas99@gmail.com>
2020-01-22 01:26:24 +0500

    only-fetch-team

--- js/fetch_team.js
+++ js/fetch_team.js
@@ -87,28 +87,3 @@ function fetchTeam(){
   };
   xhttp.send();
 }// fetchTeam()
-
-
-function delTeam(){
-  var team_id = document.getElementById("team_id").value;
-
-  if(team_id == ""){
-    alert("Enter Team ID");
-    return 0;
-  }
-
-  var xhttp = new XMLHttpRequest();
-  xhttp.open("POST", "http://192.168.1.100/SourceCode/php/delete_team.php?TID="+team_id, true);
-  xhttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-      if(this.responseText.includes("error")){
-        alert("Server-Side Error\nError: " + this.responseText);
-        return 0;
-      }
-      else{
-        alert("Success: Deleted Team No: "+team_id);
-      }
-    }
-  };
-  xhttp.send();
-}// delTeam
