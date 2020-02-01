171c14dbedcfcbdd27f02d992172b325f28982f0
Ali Faisal <alifaisalas99@gmail.com>
2020-02-01 23:18:50 +0500

    order-by-year

--- php/fetch_stats.php
+++ php/fetch_stats.php
@@ -4,7 +4,7 @@ include "connect_cms.php";
 
 $conn = openConn();
 
-$sql = "SELECT DISTINCT year_join FROM member";
+$sql = "SELECT DISTINCT year_join FROM member ORDER BY year_join ASC";
 
 $result = $conn->query($sql) or die("error: " . $conn->error);
 
@@ -60,7 +60,7 @@ foreach($year_array as $_year){
   $col["year"] = $_year;
   $col["count"] = ["frontdev"=>$frontdev_count, "backdev"=>$backdev_count, "appdev"=>$appdev_count,
   "mentor"=>$mentor_count, "mnp"=>$mnp_count, "design"=>$design_count, "content"=>$content_count, "event"=>$event_count];
-  
+
   $data[] = $col;
 }
 
