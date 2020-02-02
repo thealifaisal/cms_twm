cacd25f48862026429ede44484b040afe5c72dc2
Syed Mustufa Qadri <smustufaqadri@gmail.com>
2020-02-02 15:42:28 +0500

    Code Pushed

--- js/Faculty_Heads.js
+++
@@ -1,9 +0,0 @@
-
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("Faculty_body").innerHTML = this.responseText;
-    }
-};
-xmlhttp.open("GET","http://192.168.1.102/SourceCode/php/Faculty_Heads.php",true);
-xmlhttp.send();
--- js/Heads.js
+++
@@ -1,9 +0,0 @@
-
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("Heads").innerHTML = this.responseText;
-    }
-};
-xmlhttp.open("GET","http://192.168.1.102/SourceCode/php/Heads.php",true);
-xmlhttp.send();
